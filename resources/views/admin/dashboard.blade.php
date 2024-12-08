bb11fcb9d398adaef9967ca76108c487e041c9f7
mtauha <111292199+mtauha@users.noreply.github.com>
2024-12-08 18:19:27 +0500

    Add Courses section to admin dashboard and enhance training booking modals

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,6 +15,17 @@
             </div>
         </div>
 
+        <!-- Courses -->
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <div class="card-header">Courses</div>
+                <div class="card-body">
+                    <a href="{{ route('courses.index') }}" class="btn btn-primary btn-block">View All Courses</a>
+                    <a href="{{ route('courses.create') }}" class="btn btn-success btn-block">Add New Course</a>
+                </div>
+            </div>
+        </div>
+
         <!-- Trainings -->
         <div class="col-md-4 mb-4">
             <div class="card h-100">
@@ -31,22 +42,18 @@
             <div class="card h-100">
                 <div class="card-header">Competitions</div>
                 <div class="card-body">
-                    <a href="{{ route('competitions.index') }}" class="btn btn-primary btn-block">View All
-                        Competitions</a>
-                    <a href="{{ route('competitions.create') }}" class="btn btn-success btn-block">Add New
-                        Competition</a>
+                    <a href="{{ route('competitions.index') }}" class="btn btn-primary btn-block">View All Competitions</a>
+                    <a href="{{ route('competitions.create') }}" class="btn btn-success btn-block">Add New Competition</a>
                 </div>
             </div>
         </div>
-
 
         <!-- General Requests -->
         <div class="col-md-4 mb-4">
             <div class="card h-100">
                 <div class="card-header">General Requests</div>
                 <div class="card-body">
-                    <a href="{{ route('admin.general_requests.index') }}" class="btn btn-primary btn-block">View All
-                        General Requests</a>
+                    <a href="{{ route('admin.general_requests.index') }}" class="btn btn-primary btn-block">View All General Requests</a>
                 </div>
             </div>
         </div>
